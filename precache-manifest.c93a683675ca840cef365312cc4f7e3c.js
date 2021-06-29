0256a1659436cd31e0d3567c0d7bf13020fef618
Akshat Dobriyal <akcooldobriyal@gmail.com>
2021-06-30 00:27:38 +0530

    Updates

---
+++ precache-manifest.c93a683675ca840cef365312cc4f7e3c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83d345778350e7cb590be579aad958b7",
+    "url": "/v-share/index.html"
+  },
+  {
+    "revision": "69e93a559bd832f33f5b",
+    "url": "/v-share/static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "1a2800487a70bcbbc95f",
+    "url": "/v-share/static/css/main.cd2b5b3e.chunk.css"
+  },
+  {
+    "revision": "69e93a559bd832f33f5b",
+    "url": "/v-share/static/js/2.1ac82a9b.chunk.js"
+  },
+  {
+    "revision": "1a2800487a70bcbbc95f",
+    "url": "/v-share/static/js/main.8ad583e1.chunk.js"
+  },
+  {
+    "revision": "ba4bfe4f16500eff961e",
+    "url": "/v-share/static/js/runtime-main.0239f0ac.js"
+  },
+  {
+    "revision": "bc35086a7e4c043ac5fce48000383746",
+    "url": "/v-share/static/media/dvideobg.bc35086a.jpg"
+  },
+  {
+    "revision": "b5a88140025cf20014bc6375641f46a7",
+    "url": "/v-share/static/media/profile.b5a88140.jpeg"
+  }
+]);
